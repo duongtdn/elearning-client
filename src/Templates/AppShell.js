2f263852502028493a8337485ca62d1ac7437772
duongtdn <duongtdn@gmail.com>
2019-07-02 02:54:42 +0000

    init src structure

---
+++ src/Templates/AppShell.js
@@ -0,0 +1,16 @@
+"use strict"
+
+import React, { Component } from 'react'
+
+export default class AppShell extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h2>App Shell </h2>
+      </div>
+    )
+  }
+}
---
+++ src/scripts/app.js
@@ -0,0 +1,8 @@
+"use strict"
+
+import React from 'react'
+import { render } from 'react-dom'
+
+import AppShell from '../Templates/AppShell'
+
+render(<AppShell />, document.getElementById('root'))
