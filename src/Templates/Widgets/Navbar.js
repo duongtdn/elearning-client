e5ae9bc2a19f1d158f6f51742476f111fe6bd91a
duongtdn <duongtdn@gmail.com>
2019-07-02 09:00:43 +0000

    fix missing className w3-bar-block

--- src/Templates/Widgets/Navbar.js
+++ src/Templates/Widgets/Navbar.js
@@ -16,7 +16,7 @@ export default class Sidebar extends Component {
   }
   _renderInMediumDevice() {
     return (
-      <div className="w3-sidebar w3-black w3-border-grey w3-xlarge w3-hide-small" style={{width: '60px'}}>
+      <div className="w3-sidebar w3-bar-block w3-black w3-border-grey w3-xlarge w3-hide-small" style={{width: '60px'}}>
         <button className="w3-bar-item w3-button" onClick={e => this.props.navigate('courseOverview')}><i className="fa fa-home"/></button>
         <button className="w3-bar-item w3-button" onClick={e => this.props.navigate('studyCentral')}><i className="fa fa-tv"/></button>
         <button className="w3-bar-item w3-button" onClick={e => this.props.navigate('studyProgress')}><i className="fa fa-bar-chart"/></button>
