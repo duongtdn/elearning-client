995775c3150cd66e51b98df8b9793c18fe99d1f2
duongtdn <duongtdn@gmail.com>
2019-07-23 07:51:37 +0700

    correct path to exam app

--- src/scripts/env.js
+++ src/scripts/env.js
@@ -10,7 +10,7 @@ export default {
   app: 'dev',
   urlBasePath: '/api',
   urlQuizzesBasePath: '/api/quizzes',
-  urlExamBasePath: '/api/result',
+  urlExamBasePath: '/api/exam',
   urlTestResultBasePath: '/api/result',
   template: {
     avata: {
